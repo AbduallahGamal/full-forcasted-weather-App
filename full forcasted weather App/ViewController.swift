9031b200e836c75fd6a2c24b72a00454bd68d27e
Abduallah <abduallahgamal@gmail.com>
2019-07-28 07:10:10 +0200

    first step in project

--- full forcasted weather App/ViewController.swift
+++ full forcasted weather App/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Mark:- @IBOutlet
+    @IBOutlet weak var table: UITableView!
+    
 
+    //Mark:- Function
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
     }
+}
 
-
+//Mark:- Extension
+extension  ViewController: UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? UITableViewCell{
+            
+            cell.textLabel?.text = "Mondy"
+            return cell
+        }else{
+            return UITableViewCell()
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
 }
 
